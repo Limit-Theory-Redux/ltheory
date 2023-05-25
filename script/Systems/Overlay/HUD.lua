8538643eb6ddfc757fb57be5337297a1b66f743d
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2023-05-25 02:20:17 +0200

    feature(hud): experimental dots instead of rects

--- script/Systems/Overlay/HUD.lua
+++ script/Systems/Overlay/HUD.lua
@@ -810,6 +810,11 @@ function HUD:drawTargets (a)
         local disp = Config.game.dispoNeutral -- disposition to neutral by default
         if target:hasAttackable() and target:isAttackable() then disp = target:getDisposition(playerShip) end
 --        local c = target:getDispositionColor(disp) -- this version is preserved for future changes (esp. faction)
+        --if target:getOwner() == player then
+        --  disp = Config.game.dispoMax
+        --end
+
+        -- TODO: replace with faction relation
         local c = Disposition.GetColor(disp)
 
         if ndcMax <= 1.0 and ndc.z > 0 then
@@ -820,12 +825,17 @@ function HUD:drawTargets (a)
 
             -- Draw rounded box corners
             if target:hasAttackable() and target:isAttackable() then
+              -- TODO: add config option
+              if target:getDistance(playerShip) >= 15000 then
+                UI.DrawEx.Point(bx2-((bx2-bx1)/2), by2-((by2-by1)/2), 128, c)
+              else
               -- Innermost box shows trackable object's disposition to player
               --     (red = enemy, blue = neutral, green = friendly)
               UI.DrawEx.Wedge(bx2, by1, 4, 4, 0.125, 0.2, c, c.a)
               UI.DrawEx.Wedge(bx1, by1, 4, 4, 0.375, 0.2, c, c.a)
               UI.DrawEx.Wedge(bx1, by2, 4, 4, 0.625, 0.2, c, c.a)
               UI.DrawEx.Wedge(bx2, by2, 4, 4, 0.875, 0.2, c, c.a)
+              end
             end
             if playerTarget == target then
               -- Outermost box indicates locked target
