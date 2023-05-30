5419a4ae41c0ea7bb05f3b54d17fac58738975a4
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2023-05-31 01:24:12 +0200

    refactor: orbit radius

--- script/GameObjects/Actions/MineAt.lua
+++ script/GameObjects/Actions/MineAt.lua
@@ -11,7 +11,7 @@ local MineAt = subclass(Action, function (self, source, target, miningTimePerIte
   self.etimer = 0.0
   self.currentTime = RNG.FromTime():getInt(1, orbitTime)
   self.orbitTime = orbitTime
-  self.orbitRadius = source:getRadius() * 1.1 --TODO: replace with mining laser range later
+  self.orbitRadius = source:getRadius() --TODO: replace with mining laser range later
   self.rAxis = RNG.FromTime():getInt(1,2)
 --printf("MineAt %s from %s to %s", self.source:getYield().item:getName(), self.source:getName(), self.target:getName())
 end)
