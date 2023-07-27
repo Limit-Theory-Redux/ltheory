f911a5dc43780131adf312dcde9790a55a718ecf
Oleg Khryptul <okreptul@yahoo.com>
2023-07-27 07:33:54 +0200

    Revert "Add typedef for enums"
    This reverts commit 82a86cef1a5c7f613f34eee30b4bd6322f6e5ec2.

--- engine/lib/phx/src/input2/mod.rs
+++ engine/lib/phx/src/input2/mod.rs
@@ -1,13 +1,11 @@
 use self::mouse::MouseState;
 
 mod cursor;
-mod gamepad;
 mod keyboard;
 mod mouse;
 mod touchpad;
 
 pub use cursor::*;
-pub use gamepad::*;
 pub use keyboard::*;
 pub use mouse::*;
 pub use touchpad::*;
