82a86cef1a5c7f613f34eee30b4bd6322f6e5ec2
Oleg Khryptul <okreptul@yahoo.com>
2023-07-27 07:31:34 +0200

    Add typedef for enums

--- engine/lib/phx/src/input2/mod.rs
+++ engine/lib/phx/src/input2/mod.rs
@@ -1,11 +1,13 @@
 use self::mouse::MouseState;
 
 mod cursor;
+mod gamepad;
 mod keyboard;
 mod mouse;
 mod touchpad;
 
 pub use cursor::*;
+pub use gamepad::*;
 pub use keyboard::*;
 pub use mouse::*;
 pub use touchpad::*;
