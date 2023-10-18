bd44b319758fb3eeced83981c34abf37a79640f1
Oleg Khryptul <okreptul@yahoo.com>
2023-10-18 22:31:58 +0200

    Tip how to enable debug_refcell feature for debugging

--- engine/lib/phx/src/ui/hmgui/rf.rs
+++ engine/lib/phx/src/ui/hmgui/rf.rs
@@ -2,18 +2,30 @@ use std::cell::{Ref, RefCell, RefMut};
 use std::ops::Deref;
 use std::rc::Rc;
 
+// Note. Run with:
+//
+//    cargo +nightly r --target=x86_64-unknown-linux-gnu -Zbuild-std -Zbuild-std-features=core/debug_refcell
+//
+// to see original borrowing place on panic.
+// Change target if you are not on Linux.
+
 #[derive(Clone, Default, PartialEq, Eq)]
 pub struct Rf<T>(Rc<RefCell<T>>);
 
 impl<T> Rf<T> {
+    #[inline]
     pub fn new(value: T) -> Self {
         Self(Rc::new(RefCell::new(value)))
     }
 
+    #[track_caller]
+    #[inline]
     pub fn as_ref(&self) -> Ref<T> {
         self.0.borrow()
     }
 
+    #[track_caller]
+    #[inline]
     pub fn as_mut(&self) -> RefMut<T> {
         self.0.borrow_mut()
     }
