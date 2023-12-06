f617e19d5de173115295b2044ee8b2c808c3937a
Oleg Khryptul <okreptul@yahoo.com>
2023-12-06 22:36:03 +0100

    Apply review fixes

--- script/Enums/GuiEnums.lua
+++ script/Enums/GuiEnums.lua
@@ -3,5 +3,5 @@ Enums.Gui = {}
 -- Register script defined properties
 -- Enums.Gui.TextColorId = Gui:registerPropertyVec4("text.color", Vec4(1, 1, 1, 1))
 
--- Register an ids of the element styles
+-- Register the IDs of element styles
 -- Enums.Gui.Styles.MyButtonStyleId = Gui:getStyleId("my_button")
