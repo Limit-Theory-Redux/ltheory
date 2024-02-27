2aaf91c5e552581a7e6ef600fb0b01b5bf691d10
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2024-02-27 16:50:17 +0100

    feat: add fx volume config option

--- script/Config/Audio/AudioConfig.lua
+++ script/Config/Audio/AudioConfig.lua
@@ -4,6 +4,7 @@ Config.audio.general = {
     mainMenuMusicEnabled = true,
     soundMin             = 0,
     soundMax             = 1,   -- SetVolume range seems to go from 0 (min) to about 2 or 3 (max)
+    fxVolume             = 0.5,
     musicVolume          = 0.5, -- current volume
     mainMenu             = "LTR_Explorer.ogg",
 }
