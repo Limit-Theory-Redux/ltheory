e68513e4c94bcea0f52df62c399367edc60c856c
Oleg Khryptul <okreptul@yahoo.com>
2024-05-05 02:31:24 +0200

    feat: convert core_properties into proc macro

--- engine/lib/phx/script/meta/GuiProperties.lua
+++ engine/lib/phx/script/meta/GuiProperties.lua
@@ -1,162 +1,124 @@
 ---@meta
 
+---Core properties
 ---@enum GuiProperties
 GuiProperties = {
-    ---Config name:
-    ---opacity
-    ---Type: f32. Default value: 1
+    ---Config name: opacity
+    ---Default value: 1.0f32
     Opacity = 0,
-    ---Config name:
-    ---border-color
-    ---Type: Color. Default value: Color.TRANSPARENT
+    ---Config name: border-color
+    ---Default value: Color :: TRANSPARENT
     BorderColor = 1,
-    ---Config name:
-    ---background-color
-    ---Type: Color. Default value: Color.TRANSPARENT
+    ---Config name: background-color
+    ---Default value: Color :: TRANSPARENT
     BackgroundColor = 2,
-    ---Config name:
-    ---highlight-color
-    ---Type: Color. Default value: Color.TRANSPARENT
+    ---Config name: highlight-color
+    ---Default value: Color :: TRANSPARENT
     HighlightColor = 3,
-    ---Config name:
-    ---text.font
-    ---Type: Font. Default value: Font("Rajdhani", 14)
+    ---Config name: text.font
+    ---Default value: Font :: load("Rajdhani", 14)
     TextFont = 4,
-    ---Config name:
-    ---text.color
-    ---Type: Color. Default value: White
+    ---Config name: text.color
+    ---Default value: Color :: WHITE
     TextColor = 5,
-    ---Config name:
-    ---container.clip
-    ---Type: boolean. Default value: true
+    ---Config name: container.clip
+    ---Default value: true
     ContainerClip = 6,
-    ---Config name:
-    ---container.spacing
-    ---Type: f32. Default value: 6
+    ---Config name: container.spacing
+    ---Default value: 6.0f32
     ContainerSpacing = 7,
-    ---Config name:
-    ---container.color-frame
-    ---Type: Color. Default value: Color(0.1, 0.1, 0.1, 0.5)
+    ---Config name: container.color-frame
+    ---Default value: Color :: new(0.1, 0.1, 0.1, 0.5)
     ContainerColorFrame = 8,
-    ---Config name:
-    ---container.color-primary
-    ---Type: Color. Default value: Color(0.1, 0.5, 1.0, 1.0)
+    ---Config name: container.color-primary
+    ---Default value: Color :: new(0.1, 0.5, 1.0, 1.0)
     ContainerColorPrimary = 9,
-    ---Config name:
-    ---button.rect.opacity
-    ---Type: f32. Default value: 0.5
+    ---Config name: button.rect.opacity
+    ---Default value: 0.5f32. Maps to: Opacity
     ButtonRectOpacity = 10,
-    ---Config name:
-    ---button.rect.border-color
-    ---Type: Color. Default value: Color.TRANSPARENT. Maps to: BorderColor
+    ---Config name: button.rect.border-color
+    ---Default value: Color :: TRANSPARENT. Maps to: BorderColor
     ButtonRectBorderColor = 11,
-    ---Config name:
-    ---button.rect.background-color
-    ---Type: Color. Default value: Color(0.15, 0.15, 0.15, 0.8). Maps to: BackgroundColor
+    ---Config name: button.rect.background-color
+    ---Default value: Color :: new(0.15, 0.15, 0.15, 0.8). Maps to: BackgroundColor
     ButtonRectBackgroundColor = 12,
-    ---Config name:
-    ---button.rect.highlight-color
-    ---Type: Color. Default value: Color(0.1, 0.5, 1.0, 1.0). Maps to: HighlightColor
+    ---Config name: button.rect.highlight-color
+    ---Default value: Color :: new(0.1, 0.5, 1.0, 1.0). Maps to: HighlightColor
     ButtonRectHighlightColor = 13,
-    ---Config name:
-    ---button.text.opacity
-    ---Type: f32. Default value: 0.5
+    ---Config name: button.text.opacity
+    ---Default value: 0.5f32. Maps to: Opacity
     ButtonTextOpacity = 14,
-    ---Config name:
-    ---button.text.background-color
-    ---Type: Color. Default value: Color::TRANSPARENT. Maps to: BackgroundColor
+    ---Config name: button.text.background-color
+    ---Default value: Color :: TRANSPARENT. Maps to: BackgroundColor
     ButtonTextBackgroundColor = 15,
-    ---Config name:
-    ---button.text.highlight-color
-    ---Type: Color. Default value: Color::TRANSPARENT. Maps to: HighlightColor
+    ---Config name: button.text.highlight-color
+    ---Default value: Color :: TRANSPARENT. Maps to: HighlightColor
     ButtonTextHighlightColor = 16,
-    ---Config name:
-    ---button.text.font
-    ---Type: Font. Default value: Font("Rajdhani", 14). Maps to: TextFont
+    ---Config name: button.text.font
+    ---Default value: Font :: load("Rajdhani", 14). Maps to: TextFont
     ButtonTextFont = 17,
-    ---Config name:
-    ---button.text.color
-    ---Type: Color. Default value: White. Maps to: TextColor
+    ---Config name: button.text.color
+    ---Default value: Color :: WHITE. Maps to: TextColor
     ButtonTextColor = 18,
-    ---Config name:
-    ---checkbox.rect.opacity
-    ---Type: f32. Default value: 0.5
+    ---Config name: checkbox.rect.opacity
+    ---Default value: 0.5f32. Maps to: Opacity
     CheckboxRectOpacity = 19,
-    ---Config name:
-    ---checkbox.rect.border-color
-    ---Type: Color. Default value: Color.TRANSPARENT. Maps to: BorderColor
+    ---Config name: checkbox.rect.border-color
+    ---Default value: Color :: TRANSPARENT. Maps to: BorderColor
     CheckboxRectBorderColor = 20,
-    ---Config name:
-    ---checkbox.rect.background-color
-    ---Type: Color. Default value: Color(0.3, 0.3, 0.3, 0.5). Maps to: BackgroundColor
+    ---Config name: checkbox.rect.background-color
+    ---Default value: Color :: new(0.3, 0.3, 0.3, 0.5). Maps to: BackgroundColor
     CheckboxRectBackgroundColor = 21,
-    ---Config name:
-    ---checkbox.rect.highlight-color
-    ---Type: Color. Default value: Color(0.3, 0.3, 0.3, 1.0). Maps to: HighlightColor
+    ---Config name: checkbox.rect.highlight-color
+    ---Default value: Color :: new(0.3, 0.3, 0.3, 1.0). Maps to: HighlightColor
     CheckboxRectHighlightColor = 22,
-    ---Config name:
-    ---checkbox.text.opacity
-    ---Type: f32. Default value: 0.5
+    ---Config name: checkbox.text.opacity
+    ---Default value: 0.5f32. Maps to: Opacity
     CheckboxTextOpacity = 23,
-    ---Config name:
-    ---checkbox.text.background-color
-    ---Type: Color. Default value: Color::TRANSPARENT. Maps to: BackgroundColor
+    ---Config name: checkbox.text.background-color
+    ---Default value: Color :: TRANSPARENT. Maps to: BackgroundColor
     CheckboxTextBackgroundColor = 24,
-    ---Config name:
-    ---checkbox.text.highlight-color
-    ---Type: Color. Default value: Color::TRANSPARENT. Maps to: HighlightColor
+    ---Config name: checkbox.text.highlight-color
+    ---Default value: Color :: TRANSPARENT. Maps to: HighlightColor
     CheckboxTextHighlightColor = 25,
-    ---Config name:
-    ---checkbox.text.font
-    ---Type: Font. Default value: Font("Rajdhani", 14). Maps to: TextFont
+    ---Config name: checkbox.text.font
+    ---Default value: Font :: load("Rajdhani", 14). Maps to: TextFont
     CheckboxTextFont = 26,
-    ---Config name:
-    ---checkbox.text.color
-    ---Type: Color. Default value: White. Maps to: TextColor
+    ---Config name: checkbox.text.color
+    ---Default value: Color :: WHITE. Maps to: TextColor
     CheckboxTextColor = 27,
-    ---Config name:
-    ---checkbox.click-area.border-color
-    ---Type: Color. Default value: Color(0.1, 0.1, 0.1, 0.5). Maps to: BorderColor
+    ---Config name: checkbox.click-area.border-color
+    ---Default value: Color :: new(0.1, 0.1, 0.1, 0.5). Maps to: BorderColor
     CheckboxClickAreaBorderColor = 28,
-    ---Config name:
-    ---checkbox.click-area.background-color
-    ---Type: Color. Default value: Color::TRANSPARENT. Maps to: BackgroundColor
+    ---Config name: checkbox.click-area.background-color
+    ---Default value: Color :: TRANSPARENT. Maps to: BackgroundColor
     CheckboxClickAreaBackgroundColor = 29,
-    ---Config name:
-    ---checkbox.click-area.highlight-color
-    ---Type: Color. Default value: Color::TRANSPARENT. Maps to: HighlightColor
+    ---Config name: checkbox.click-area.highlight-color
+    ---Default value: Color :: TRANSPARENT. Maps to: HighlightColor
     CheckboxClickAreaHighlightColor = 30,
-    ---Config name:
-    ---checkbox.click-area.selected-color
-    ---Type: Color. Default value: Color(0.1, 0.5, 1.0, 1.0)
+    ---Config name: checkbox.click-area.selected-color
+    ---Default value: Color :: new(0.1, 0.5, 1.0, 1.0)
     CheckboxClickAreaSelectedColor = 31,
-    ---Config name:
-    ---scroll-area.hscroll.show
-    ---Type: boolean. Default value: true
+    ---Config name: scroll-area.hscroll.show
+    ---Default value: true
     ScrollAreaHScrollShow = 32,
-    ---Config name:
-    ---scroll-area.vscroll.show
-    ---Type: boolean. Default value: true
+    ---Config name: scroll-area.vscroll.show
+    ---Default value: true
     ScrollAreaVScrollShow = 33,
-    ---Config name:
-    ---scroll-area.scrollbar.length
-    ---Type: f32. Default value: 4
+    ---Config name: scroll-area.scrollbar.length
+    ---Default value: 4f32
     ScrollAreaScrollbarLength = 34,
-    ---Config name:
-    ---scroll-area.scrollbar.background-color
-    ---Type: Color. Default value: Color(0.3, 0.3, 0.3, 0.3)
+    ---Config name: scroll-area.scrollbar.background-color
+    ---Default value: Color :: new(0.3, 0.3, 0.3, 0.3)
     ScrollAreaScrollbarBackgroundColor = 35,
-    ---Config name:
-    ---scroll-area.scroll-scale
-    ---Type: f32. Default value: 20
+    ---Config name: scroll-area.scroll-scale
+    ---Default value: 20f32
     ScrollAreaScrollScale = 36,
-    ---Config name:
-    ---scroll-area.scrollbar.visibility-stable-time
-    ---Type: u64. Default value: 400
+    ---Config name: scroll-area.scrollbar.visibility-stable-time
+    ---Default value: 400u64
     ScrollAreaScrollbarVisibilityStableTime = 37,
-    ---Config name:
-    ---scroll-area.scrollbar.visibility-fade-time
-    ---Type: u64. Default value: 200
+    ---Config name: scroll-area.scrollbar.visibility-fade-time
+    ---Default value: 200u64
     ScrollAreaScrollbarVisibilityFadeTime = 38,
 }
 
--- engine/lib/phx/src/ui/hmgui/core_properties.rs
+++ engine/lib/phx/src/ui/hmgui/core_properties.rs
@@ -4,122 +4,50 @@ use crate::render::{Color, Font};
 
 use super::{HmGuiProperty, HmGuiPropertyId, HmGuiPropertyInfo};
 
-macro_rules! core_properties {
-    ($(
-        $(#[doc = $doc:literal])*
-        ($v:ident, $n:literal, $d:expr $(, $m:ident)*),
-    )*) => {
-        #[luajit_ffi_gen::luajit_ffi(name = "GuiProperties")]
-        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-        pub enum HmGuiProperties {
-            $(
-                #[doc = "Config name: "]
-                #[doc = $n]
-                $(#[doc = $doc])*
-                $v
-            ),*
-        }
-
-        // NOTE: it's not possible to implement Deref because of recursive call
-        impl HmGuiProperties {
-            pub fn id(&self) -> usize {
-                *self as _
-            }
-        }
-
-        pub fn register_core_properties() -> IndexMap<String, HmGuiPropertyInfo> {
-            let mut r = Default::default();
-
-            $(reg(&mut r, $n, $d, HmGuiProperties::$v, &[$((HmGuiProperties::$m as usize).into(),)*]);)*
-
-            r
-        }
-    };
-}
-
 // Property id, name, value and optional list of mapped property ids
-core_properties! {
-    /// Type: f32. Default value: 1
+phx_macros::core_properties! {
     (Opacity,               "opacity",                 1.0f32),
-    /// Type: Color. Default value: Color.TRANSPARENT
     (BorderColor,           "border-color",            Color::TRANSPARENT),
-    /// Type: Color. Default value: Color.TRANSPARENT
     (BackgroundColor,       "background-color",        Color::TRANSPARENT),
-    /// Type: Color. Default value: Color.TRANSPARENT
     (HighlightColor,        "highlight-color",         Color::TRANSPARENT),
-    /// Type: Font. Default value: Font("Rajdhani", 14)
     (TextFont,              "text.font",               Font::load("Rajdhani", 14)),
-    /// Type: Color. Default value: White
     (TextColor,             "text.color",              Color::WHITE),
 
-    /// Type: boolean. Default value: true
     (ContainerClip,         "container.clip",          true),
-    /// Type: f32. Default value: 6
     (ContainerSpacing,      "container.spacing",       6.0f32),
-    /// Type: Color. Default value: Color(0.1, 0.1, 0.1, 0.5)
     (ContainerColorFrame,   "container.color-frame",   Color::new(0.1, 0.1, 0.1, 0.5)),
-    /// Type: Color. Default value: Color(0.1, 0.5, 1.0, 1.0)
     (ContainerColorPrimary, "container.color-primary", Color::new(0.1, 0.5, 1.0, 1.0)),
 
-    /// Type: f32. Default value: 0.5
     (ButtonRectOpacity,         "button.rect.opacity",          0.5f32, Opacity),
-    /// Type: Color. Default value: Color.TRANSPARENT. Maps to: BorderColor
     (ButtonRectBorderColor,     "button.rect.border-color",     Color::TRANSPARENT, BorderColor),
-    /// Type: Color. Default value: Color(0.15, 0.15, 0.15, 0.8). Maps to: BackgroundColor
     (ButtonRectBackgroundColor, "button.rect.background-color", Color::new(0.15, 0.15, 0.15, 0.8), BackgroundColor),
-    /// Type: Color. Default value: Color(0.1, 0.5, 1.0, 1.0). Maps to: HighlightColor
     (ButtonRectHighlightColor,  "button.rect.highlight-color",  Color::new(0.1, 0.5, 1.0, 1.0), HighlightColor),
-    /// Type: f32. Default value: 0.5
     (ButtonTextOpacity,         "button.text.opacity",          0.5f32, Opacity),
-    /// Type: Color. Default value: Color::TRANSPARENT. Maps to: BackgroundColor
     (ButtonTextBackgroundColor, "button.text.background-color", Color::TRANSPARENT, BackgroundColor),
-    /// Type: Color. Default value: Color::TRANSPARENT. Maps to: HighlightColor
     (ButtonTextHighlightColor,  "button.text.highlight-color",  Color::TRANSPARENT, HighlightColor),
-    /// Type: Font. Default value: Font("Rajdhani", 14). Maps to: TextFont
     (ButtonTextFont,            "button.text.font",             Font::load("Rajdhani", 14), TextFont),
-    /// Type: Color. Default value: White. Maps to: TextColor
     (ButtonTextColor,           "button.text.color",            Color::WHITE, TextColor),
 
-    /// Type: f32. Default value: 0.5
     (CheckboxRectOpacity,          "checkbox.rect.opacity",          0.5f32, Opacity),
-    /// Type: Color. Default value: Color.TRANSPARENT. Maps to: BorderColor
     (CheckboxRectBorderColor,      "checkbox.rect.border-color",     Color::TRANSPARENT, BorderColor),
-    /// Type: Color. Default value: Color(0.3, 0.3, 0.3, 0.5). Maps to: BackgroundColor
     (CheckboxRectBackgroundColor,  "checkbox.rect.background-color", Color::new(0.3, 0.3, 0.3, 0.5), BackgroundColor),
-    /// Type: Color. Default value: Color(0.3, 0.3, 0.3, 1.0). Maps to: HighlightColor
     (CheckboxRectHighlightColor,   "checkbox.rect.highlight-color",  Color::new(0.3, 0.3, 0.3, 1.0), HighlightColor),
-    /// Type: f32. Default value: 0.5
     (CheckboxTextOpacity,          "checkbox.text.opacity",          0.5f32, Opacity),
-    /// Type: Color. Default value: Color::TRANSPARENT. Maps to: BackgroundColor
     (CheckboxTextBackgroundColor,  "checkbox.text.background-color", Color::TRANSPARENT, BackgroundColor),
-    /// Type: Color. Default value: Color::TRANSPARENT. Maps to: HighlightColor
     (CheckboxTextHighlightColor,   "checkbox.text.highlight-color",  Color::TRANSPARENT, HighlightColor),
-    /// Type: Font. Default value: Font("Rajdhani", 14). Maps to: TextFont
     (CheckboxTextFont,             "checkbox.text.font",             Font::load("Rajdhani", 14), TextFont),
-    /// Type: Color. Default value: White. Maps to: TextColor
     (CheckboxTextColor,            "checkbox.text.color",            Color::WHITE, TextColor),
-    /// Type: Color. Default value: Color(0.1, 0.1, 0.1, 0.5). Maps to: BorderColor
     (CheckboxClickAreaBorderColor,     "checkbox.click-area.border-color",     Color::new(0.1, 0.1, 0.1, 0.5), BorderColor),
-    /// Type: Color. Default value: Color::TRANSPARENT. Maps to: BackgroundColor
     (CheckboxClickAreaBackgroundColor, "checkbox.click-area.background-color", Color::TRANSPARENT, BackgroundColor),
-    /// Type: Color. Default value: Color::TRANSPARENT. Maps to: HighlightColor
     (CheckboxClickAreaHighlightColor,  "checkbox.click-area.highlight-color",  Color::TRANSPARENT, HighlightColor),
-    /// Type: Color. Default value: Color(0.1, 0.5, 1.0, 1.0)
     (CheckboxClickAreaSelectedColor,   "checkbox.click-area.selected-color",   Color::new(0.1, 0.5, 1.0, 1.0)),
 
-    /// Type: boolean. Default value: true
     (ScrollAreaHScrollShow,                   "scroll-area.hscroll.show", true),
-    /// Type: boolean. Default value: true
     (ScrollAreaVScrollShow,                   "scroll-area.vscroll.show", true),
-    /// Type: f32. Default value: 4
     (ScrollAreaScrollbarLength,               "scroll-area.scrollbar.length", 4f32),
-    /// Type: Color. Default value: Color(0.3, 0.3, 0.3, 0.3)
     (ScrollAreaScrollbarBackgroundColor,      "scroll-area.scrollbar.background-color", Color::new(0.3, 0.3, 0.3, 0.3)),
-    /// Type: f32. Default value: 20
     (ScrollAreaScrollScale,                   "scroll-area.scroll-scale", 20f32),
-    /// Type: u64. Default value: 400
     (ScrollAreaScrollbarVisibilityStableTime, "scroll-area.scrollbar.visibility-stable-time", 400u64),
-    /// Type: u64. Default value: 200
     (ScrollAreaScrollbarVisibilityFadeTime,   "scroll-area.scrollbar.visibility-fade-time", 200u64),
 }
 
