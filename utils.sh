9fc78ac47a103347534a360f6192ca02a237ed3a
David Avedissian <git@dga.dev>
2024-05-28 00:52:02 +0100

    Clean up utils.sh

--- utils.sh
+++ utils.sh
@@ -1,6 +1,6 @@
 # Create a macOS app bundle in the 'bin' directory.
 function create_app_bundle {
-    echo "Making bin/Limit Theory Redux.app..." 
+    echo "Creating app bundle './bin/Limit Theory Redux.app'"
 
     local destination
     destination="bin"
@@ -26,21 +26,18 @@ function create_app_bundle {
     }
 EOF
 
-    echo "Copying data files ..."
+    echo "Copying data files..."
     rsync -Ca ./res "$destination/Limit Theory Redux.app/Contents/"
     rsync -Ca ./script "$destination/Limit Theory Redux.app/Contents/"
     mkdir -p "$destination/Limit Theory Redux.app/Contents/engine/lib/phx"
     rsync -Ca ./engine/lib/phx/script "$destination/Limit Theory Redux.app/Contents/engine/lib/phx"
 
-    echo "Copying binary ..."
+    echo "Copying binary..."
     cp -a "${target_dir}/ltr${binsuffix}" "$destination/Limit Theory Redux.app/Contents/MacOS/ltr"
 
-    echo "Copying and fixing dynamic libraries... "
+    echo "Copying dynamic libraries..."
     cp "${target_dir}/deps/${libprefix}phx${libsuffix}" "$destination/Limit Theory Redux.app/Contents/MacOS/${libprefix}phx${libsuffix}"
 
-    echo "Re-sign libraries with an 'ad-hoc signing' see man codesign"
-    # codesign --sign - --force $destination/Limit Theory Redux.app/Contents/libs/*
-
-    echo "Stripping binary ..."
+    echo "Stripping binary..."
     strip -u -r "$destination/Limit Theory Redux.app/Contents/MacOS/ltr"
 }
