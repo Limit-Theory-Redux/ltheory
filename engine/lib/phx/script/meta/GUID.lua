deadb2765e3a0d09259c88330fb62ad6cc22000f
Philip Pugh <philip.pugh@gmail.com>
2024-06-29 14:58:13 +0100

    Refactor GUID using luajit_ffi_gen::luajit_ffi macro

---
+++ engine/lib/phx/script/meta/GUID.lua
@@ -0,0 +1,14 @@
+---@meta
+
+---@class GUID
+GUID = {}
+
+---@return integer
+function GUID.Create() end
+
+---@param id integer
+---@return boolean
+function GUID.Exists(id) end
+
+function GUID.Reset() end
+
--- engine/lib/phx/src/system/guid.rs
+++ engine/lib/phx/src/system/guid.rs
@@ -2,17 +2,21 @@ use std::sync::atomic::{AtomicU64, Ordering};
 
 static NEXT_ID: AtomicU64 = AtomicU64::new(1);
 
-#[no_mangle]
-pub extern "C" fn GUID_Create() -> u64 {
-    NEXT_ID.fetch_add(1, Ordering::Relaxed)
+#[repr(C)]
+pub struct GUID {
 }
 
-#[no_mangle]
-pub extern "C" fn GUID_Exists(id: u64) -> bool {
-    id < NEXT_ID.load(Ordering::Relaxed) && id != 0
-}
+#[luajit_ffi_gen::luajit_ffi]
+impl GUID {
+    pub fn create() -> u64 {
+        NEXT_ID.fetch_add(1, Ordering::Relaxed)
+    }
 
-#[no_mangle]
-pub extern "C" fn GUID_Reset() {
-    NEXT_ID.store(1, Ordering::Relaxed);
+    pub fn exists(id: u64) -> bool {
+        id < NEXT_ID.load(Ordering::Relaxed) && id != 0
+    }
+
+    pub fn reset() {
+        NEXT_ID.store(1, Ordering::Relaxed);
+    }
 }
