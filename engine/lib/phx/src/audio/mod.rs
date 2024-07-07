20cc97be20cbfa04de4195070ecd33f8502aa0c7
David Avedissian <git@dga.dev>
2024-07-07 18:42:16 +0100

    Update Kira to 0.9, which removes the command buffer full issues.

--- engine/lib/phx/src/audio/mod.rs
+++ engine/lib/phx/src/audio/mod.rs
@@ -5,16 +5,3 @@ mod sound_instance;
 pub use audio::*;
 pub use sound::*;
 pub use sound_instance::*;
-
-use kira::CommandError;
-use tracing::debug;
-
-pub(crate) fn process_command_error(res: Result<(), CommandError>, msg: &str) {
-    match res {
-        Ok(_) => {}
-        Err(CommandError::CommandQueueFull) => {
-            debug!("{msg}. Command queue is full");
-        }
-        Err(err) => panic!("{msg}. Error: {err}"),
-    }
-}
