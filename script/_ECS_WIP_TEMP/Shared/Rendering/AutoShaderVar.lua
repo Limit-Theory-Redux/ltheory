b61765751195750f754feef287052d32a010e60d
LukeJHarvey <Lukeharvey0226@gmail.com>
2024-08-17 02:42:03 -0700

    minor changes

--- script/_ECS_WIP_TEMP/Shared/Rendering/AutoShaderVar.lua
+++ script/_ECS_WIP_TEMP/Shared/Rendering/AutoShaderVar.lua
@@ -1,18 +1,18 @@
 ---@class AutoShaderVar
 ---@field uniformName string
 ---@field uniformInt integer
----@field renderFn function
+---@field callbackFn function
 
 ---@param self AutoShaderVar
 ---@param uniformName string
----@param renderFn function
+---@param callbackFn function
 ---@class AutoShaderVar
-local AutoShaderVar = Class(function(self, uniformName, renderFn)
+local AutoShaderVar = Class(function(self, uniformName, callbackFn)
     ---@cast self AutoShaderVar
     self.uniformName = uniformName
     -- self.params = params
     self.uniformInt = -1 -- -1 = unset
-    self.renderFn = renderFn
+    self.callbackFn = callbackFn
 end)
 
 ---@param uniformInt integer
@@ -22,8 +22,8 @@ end
 
 --!temp NOT FINAL IMPLEMENTATION, FOR TESTING
 ---@param shaderState ShaderState
-function AutoShaderVar:render(shaderState, ...)
-    self.renderFn(shaderState, self.uniformInt, ...)
+function AutoShaderVar:setShaderVar(shaderState, ...)
+    self.callbackFn(shaderState, self.uniformInt, ...)
 end
 
 return AutoShaderVar
