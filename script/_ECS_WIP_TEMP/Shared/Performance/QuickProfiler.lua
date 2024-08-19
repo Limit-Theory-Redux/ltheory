b87a0767520ca8039c5972b65adba95167c4f70f
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2024-08-19 22:35:34 +0200

    refactor(utilities): quickprofiler cleanup

--- script/_ECS_WIP_TEMP/Shared/Performance/QuickProfiler.lua
+++ script/_ECS_WIP_TEMP/Shared/Performance/QuickProfiler.lua
@@ -6,30 +6,36 @@ local QuickProfiler = Class(function(self, name, enable, withMovingAverage)
 end)
 
 function QuickProfiler:init(name, enable, withMovingAverage)
+    ---@private
     self.name = name or "Undefined"
+    ---@private
     self.enabled = enable or false
+    ---@private
     self.times = {}
+    ---@private
     self.withMovingAverage = withMovingAverage
 end
 
 function QuickProfiler:start()
     if self.enabled then
-        self.start = TimeStamp.Now()
+        ---@private
+        self.startTime = TimeStamp.Now()
 
-        if self.withMovingAverage and not self.movingAverageStart then
-            self.movingAverageStart = TimeStamp.Now()
+        if self.withMovingAverage and not self.movingAverageStartTime then
+            ---@private
+            self.movingAverageStartTime = TimeStamp.Now()
         end
     end
 end
 
 function QuickProfiler:stop()
-    if self.enabled and self.start then
+    if self.enabled and self.startTime then
         local stop = TimeStamp.Now()
-        local diff = self.start:getDifference(stop)
+        local diff = self.startTime:getDifference(stop)
         insert(self.times, diff)
         Log.Debug(format("%s, %.3f ms", self.name, diff * 1000))
 
-        if self.withMovingAverage and self.movingAverageStart:getDifference(stop) >= 1.0 then
+        if self.withMovingAverage and self.movingAverageStartTime:getDifference(stop) >= 1.0 then
             local total = 0
             for _, time in ipairs(self.times) do
                 total = total + time
@@ -42,10 +48,10 @@ function QuickProfiler:stop()
             Log.Debug(self.name .. ", Percentage of 60FPS goal frametime: " .. format("%.2f", totalMS / 16.67 * 100) .. "%%") -- hack to make %% work with Log.Debug´s string.format()
             Log.Debug(self.name .. ", Percentage of 120FPS goal frametime: " .. format("%.2f", totalMS / 8.33 * 100) .. "%%") -- hack to make %% work with Log.Debug´s string.format()
 
-            self.times = {}
-            self.movingAverageStart = nil
+            table.clear(self.times)
+            self.movingAverageStartTime = nil
         end
-        self.start = nil
+        self.startTime = nil
     end
 end
 
