e5e5815bbd57b7a3f2b74d2919562ef21a83507a
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2024-08-20 22:17:49 +0200

    feat(ecs): game view system skeleton

---
+++ script/_ECS_WIP_TEMP/Systems/Rendering/GameViewSystem.lua
@@ -0,0 +1,27 @@
+--todo: render white screen
+-- Systems
+local GlobalStorage = require("_ECS_WIP_TEMP.Systems.GlobalStorage") --!temp path
+
+-- Utilities
+local QuickProfiler = require("_ECS_WIP_TEMP.Shared.Performance.QuickProfiler")
+
+---@class GameViewSystem
+---@overload fun(self: GameViewSystem) class internal
+---@overload fun() class external
+local GameViewSystem = Class(function(self)
+    ---@diagnostic disable-next-line: invisible
+    self:registerVars()
+    ---@diagnostic disable-next-line: invisible
+    self:registerEvents()
+end)
+
+---@private
+function GameViewSystem:registerVars()
+    self.profiler = QuickProfiler("GameViewSystem", false, false)
+end
+
+---@private
+function GameViewSystem:registerEvents()
+end
+
+return GameViewSystem()
