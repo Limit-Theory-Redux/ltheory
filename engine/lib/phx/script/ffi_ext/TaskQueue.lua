bae37a58f740da8f2f9db39875865170d664c186
David Avedissian <git@dga.dev>
2024-09-02 20:54:56 +0100

    When transferring ownership of the initial payload to Rust, make sure to disable the finalizer first.

--- engine/lib/phx/script/ffi_ext/TaskQueue.lua
+++ engine/lib/phx/script/ffi_ext/TaskQueue.lua
@@ -2,7 +2,7 @@ local libphx = require('libphx').lib
 
 function onDef_TaskQueue_t(t, mt)
     mt.__index.sendTask = function(self, workerId, data)
-        local taskIdPtr = libphx.TaskQueue_SendTask(self, workerId, PayloadConverter:valueToPayload(data, true))
+        local taskIdPtr = libphx.TaskQueue_SendTask(self, workerId, ffi.gc(PayloadConverter:valueToPayload(data, true), nil))
         return taskIdPtr[1]
     end
 
