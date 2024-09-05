1f0cb84f79d4cf87dbdcf02e53677eb6bc4c2a3c
Oleg Khryptul <okreptul@yahoo.com>
2024-09-05 21:58:14 +0200

    Add comment about fix of double free problem

--- engine/lib/phx/script/ffi_ext/TaskQueue.lua
+++ engine/lib/phx/script/ffi_ext/TaskQueue.lua
@@ -2,7 +2,10 @@ local libphx = require('libphx').lib
 
 function onDef_TaskQueue_t(t, mt)
     mt.__index.sendTask = function(self, workerId, data)
-        local taskIdPtr = libphx.TaskQueue_SendTask(self, workerId, ffi.gc(PayloadConverter:valueToPayload(data, true), nil))
+        local payload = PayloadConverter:valueToPayload(data, true)
+        -- Call ffi.gc here to avoid double free issue. See conversation here:
+        -- https://discord.com/channels/695088786702336000/1265576869856542760/1280255882038607972
+        local taskIdPtr = libphx.TaskQueue_SendTask(self, workerId, ffi.gc(payload, nil))
         return taskIdPtr[1]
     end
 
