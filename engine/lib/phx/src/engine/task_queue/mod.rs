42c291824103a03f12b410e9a8209a33fd9d37b4
Oleg Khryptul <okreptul@yahoo.com>
2024-09-06 18:41:01 +0200

    Worker stub

--- engine/lib/phx/src/engine/task_queue/mod.rs
+++ engine/lib/phx/src/engine/task_queue/mod.rs
@@ -2,6 +2,7 @@ mod error;
 mod task_queue;
 mod task_result;
 mod worker_data;
+mod worker;
 mod worker_id;
 mod worker_thread;
 
@@ -9,5 +10,6 @@ pub use error::*;
 pub use task_queue::*;
 pub use task_result::*;
 pub use worker_data::*;
+pub use worker::*;
 pub use worker_id::*;
 pub use worker_thread::*;
