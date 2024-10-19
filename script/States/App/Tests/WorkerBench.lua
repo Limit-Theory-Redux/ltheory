9c9ec88df8cf439f0838750dd6cd8838ffc5a49b
Oleg Khryptul <okreptul@yahoo.com>
2024-10-19 22:26:43 +0200

    Add complex worker test

--- script/States/App/Tests/WorkerBench.lua
+++ script/States/App/Tests/WorkerBench.lua
@@ -1,38 +1,21 @@
 local WorkerBench = require('States.Application')
 
-local function removeElement(t, value)
-    for i, v in ipairs(t) do
-        if v == value then
-            table.remove(t, i)
-            return
-        end
-    end
-    for i, v in ipairs(t) do
-        print(tostring(i) .. ": " .. tostring(v))
-    end
-    Log.Error("Cannot find table value " .. tostring(value))
-end
-
 function WorkerBench:onInit()
     Log.Info("WorkerBench:onInit: Start")
 
     local instancesCount = 4
-    local messagesCount = 1000
+    local messagesCount = 10000
     local taskIds = {}
 
     Profiler.Enable()
     Profiler.Begin('WorkerBench')
 
     WorkerId.AddWorkers({ "TestWorker" })
-    if TaskQueue:startWorker(WorkerId.TestWorker, "TestWorker", "script/States/App/Tests/TestWorkerFunction.lua", instancesCount) == false then
-        Log.Error("Cannot start worker")
-    end
+
+    TaskQueue:startWorker(WorkerId.TestWorker, "TestWorker", "script/States/App/Tests/TestWorkerFunction.lua", instancesCount)
 
     for i = 1, messagesCount do
         local taskId = TaskQueue:sendTask(WorkerId.TestWorker, "TestPayload")
-        if taskId == nil then
-            Log.Error("Cannot send task " .. i)
-        end
         table.insert(taskIds, taskId)
         -- Log.Debug("New task: " .. tostring(taskId))
     end
@@ -43,7 +26,7 @@ function WorkerBench:onInit()
         local taskId, _ = TaskQueue:nextTaskResult(WorkerId.TestWorker)
         if taskId ~= nil then
             -- Log.Debug("Received: " .. tostring(taskId))
-            removeElement(taskIds, taskId)
+            table.removeValue(taskIds, taskId)
             -- Log.Debug("Left: " .. tostring(#taskIds))
         end
     end
--- script/States/App/Tests/WorkerTest.lua
+++ script/States/App/Tests/WorkerTest.lua
@@ -3,15 +3,13 @@ local WorkerTest = require('States.Application')
 function WorkerTest:onInit()
     Log.Info("WorkerTest:onInit: Start")
 
-    WorkerId.AddWorkers({ "TestWorker" })
-    if TaskQueue:startWorker(WorkerId.TestWorker, "TestWorker", "script/States/App/Tests/TestWorkerFunction.lua", 1) == false then
-        Log.Error("Cannot start worker")
-    end
-    local expectedTaskId = TaskQueue:sendTask(WorkerId.TestWorker, "TestPayload")
-    if expectedTaskId == nil then
-        Log.Error("Cannot send task")
-    end
+    WorkerId.AddWorkers({ "TestWorker", "TestWorker2" })
 
+    TaskQueue:startWorker(WorkerId.TestWorker, "TestWorker", "script/States/App/Tests/TestWorkerFunction.lua", 1)
+    TaskQueue:startWorker(WorkerId.TestWorker2, "TestWorker2", "script/States/App/Tests/TestWorkerFunction2.lua", 1)
+
+    -- Simple test
+    local expectedTaskId = TaskQueue:sendTask(WorkerId.TestWorker, "TestPayload")
     local taskId, payload = TaskQueue:nextTaskResult(WorkerId.TestWorker)
     while taskId == nil do
         taskId, payload = TaskQueue:nextTaskResult(WorkerId.TestWorker)
@@ -19,6 +17,29 @@ function WorkerTest:onInit()
 
     assert(expectedTaskId == taskId, "Expected " .. tostring(expectedTaskId) .. " but was " .. tostring(taskId))
     assert(payload == "TestPayload_OUT", "Expected 'TestPayload_OUT' but was '" .. payload .. "'")
+
+    -- Complex test
+    local expectedPayload = {
+        boolVal = true,
+        intVal = 3,
+        floatVal = 4.0,
+        strVal = "TestPayload2",
+        tableVal = {
+            boolVal = true,
+            intVal = 5,
+            floatVal = 6.0,
+            strVal = "TestPayload3",
+        }
+    }
+    local expectedTaskId = TaskQueue:sendTask(WorkerId.TestWorker2, expectedPayload)
+    local taskId, payload = TaskQueue:nextTaskResult(WorkerId.TestWorker2)
+    while taskId == nil do
+        taskId, payload = TaskQueue:nextTaskResult(WorkerId.TestWorker2)
+    end
+
+    assert(expectedTaskId == taskId, "Expected " .. tostring(expectedTaskId) .. " but was " .. tostring(taskId))
+    assert(table.equal(payload, expectedPayload, true),
+        "Expected '" .. table.tostring(expectedPayload, true) .. "' but was '" .. table.tostring(payload, true) .. "'")
 
     TaskQueue:stopAllWorkers()
 
