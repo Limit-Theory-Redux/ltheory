4981661f01c9a1db6c46b360cab9c641146bb680
David Avedissian <git@dga.dev>
2024-11-03 00:18:47 +0000

    Move to new cargo build syntax, and add the link search path for libphx.
    Rust 1.81 seemed to change the linker paths, so was failing to link
    against the phx library. Potentally a change to the working directory.
    To fix this, this commit explicitly adds the linker path to the phx
    library using cargo::rustc-link-search.
    
    At the same time, as our MSV is >1.77, we can move to the _new_ Cargo
    build syntax: cargo::<key>

--- engine/bin/ltr/build.rs
+++ engine/bin/ltr/build.rs
@@ -5,7 +5,7 @@ use winres::WindowsResource;
 
 fn main() {
     if env::var("TARGET")
-        .expect("target")
+        .expect("target is not set")
         .ends_with("windows-msvc")
     {
         // App Icon
@@ -15,28 +15,38 @@ fn main() {
             .unwrap();
 
         let manifest = Path::new("src/ltr.exe.manifest").canonicalize().unwrap();
-        println!("cargo:rustc-link-arg-bins=/MANIFEST:EMBED");
+        println!("cargo::rustc-link-arg-bins=/MANIFEST:EMBED");
         println!(
             "cargo:rustc-link-arg-bins=/MANIFESTINPUT:{}",
             manifest.display()
         );
-        println!("cargo:rerun-if-changed=src/ltr.exe.manifest");
+        println!("cargo::rerun-if-changed=src/ltr.exe.manifest");
 
         // Prefer dedicated graphics.
-        println!("cargo:rustc-link-arg=/EXPORT:NvOptimusEnablement");
-        println!("cargo:rustc-link-arg=/EXPORT:AmdPowerXpressRequestHighPerformance");
+        println!("cargo::rustc-link-arg=/EXPORT:NvOptimusEnablement");
+        println!("cargo::rustc-link-arg=/EXPORT:AmdPowerXpressRequestHighPerformance");
     }
 
     if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
-        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/deps");
+        println!("cargo::rustc-link-arg=-Wl,-rpath,$ORIGIN");
+        println!("cargo::rustc-link-arg=-Wl,-rpath,$ORIGIN/deps");
     } else if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path");
-        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/deps");
+        println!("cargo::rustc-link-arg=-Wl,-rpath,@executable_path");
+        println!("cargo::rustc-link-arg=-Wl,-rpath,@executable_path/deps");
     }
 
-    // If we don't specify rerun-if-changed, then Cargo will always rerun build.rs, causing phx to always be rebuild.
+    // libphx is linked with the #[link] attribute in main.rs. The phx library is placed in the
+    // `target` directory, so we need to add that to the linker search path.
     //
-    // Here, we just specify build.rs as the file that should be used to guide if phx's build script needs to be re-run.
-    println!("cargo:rerun-if-changed=build.rs");
+    // OUT_DIR is set to <src>/target/<cfg>/build/ltr-<hash>/out, so we need to move up 3
+    // directories from there.
+    let dir = env::var("OUT_DIR").unwrap();
+    println!("cargo::rustc-link-search={}", Path::new(&dir).join("../../..").display());
+
+    // If we don't specify rerun-if-changed, then Cargo will always rerun build.rs, causing phx to
+    // always be rebuilt.
+    //
+    // Here, we just specify build.rs as the file that should be used to guide if phx's build
+    // script needs to be re-run.
+    println!("cargo::rerun-if-changed=build.rs");
 }
