bb1c6bf3e50f71d26b4aa07bf03bf85f042119a7
Oleg Khryptul <okreptul@yahoo.com>
2025-04-08 23:01:47 +0200

    Disable clippy warnings in bindings.rs

--- engine/lib/phx/src/render/gl_bindings.rs
+++ engine/lib/phx/src/render/gl_bindings.rs
@@ -1,10 +1,10 @@
 pub mod gl {
     #![allow(clippy::missing_transmute_annotations)] // disable clippy warnings in the generated bindings.rs file
     #![allow(unsafe_code)] // TODO: remove
+    #![allow(clippy::unused_unit)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-    #[allow(clippy::unused_unit)]
     pub fn error_to_str(err: types::GLenum) -> &'static str {
         match err {
             INVALID_ENUM => "GL_INVALID_ENUM",
