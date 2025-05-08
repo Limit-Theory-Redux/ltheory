71a38c594ad6a4d5a0e0ada497c6ad86b241fac4
Oleg Khryptul <okreptul@yahoo.com>
2025-05-09 00:12:24 +0200

    Edition 2024

--- engine/lib/phx/src/system/memory.rs
+++ engine/lib/phx/src/system/memory.rs
@@ -1,43 +1,43 @@
 #![allow(non_snake_case)] // TODO: remove this and fix all warnings
 #![allow(unsafe_code)] // TODO: refactor
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn Memory_Alloc(size: usize) -> *mut libc::c_void {
-    libc::malloc(size)
+    unsafe { libc::malloc(size) }
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn Memory_Calloc(n: usize, size: usize) -> *mut libc::c_void {
-    libc::calloc(n, size)
+    unsafe { libc::calloc(n, size) }
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn Memory_Free(ptr: *mut libc::c_void) {
-    libc::free(ptr);
+    unsafe { libc::free(ptr) };
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn Memory_MemCopy(
     dst: *mut libc::c_void,
     src: *const libc::c_void,
     size: usize,
 ) {
-    libc::memcpy(dst, src, size);
+    unsafe { libc::memcpy(dst, src, size) };
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn Memory_MemMove(
     dst: *mut libc::c_void,
     src: *const libc::c_void,
     size: usize,
 ) {
-    libc::memmove(dst, src, size);
+    unsafe { libc::memmove(dst, src, size) };
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn Memory_Realloc(
     ptr: *mut libc::c_void,
     new_size: usize,
 ) -> *mut libc::c_void {
-    libc::realloc(ptr, new_size)
+    unsafe { libc::realloc(ptr, new_size) }
 }
