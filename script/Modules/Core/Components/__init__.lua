a59fd003575c942119b0e09c956aa2543678d547
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2025-10-29 15:47:16 +0100

    feat(tags): add tags

--- script/Modules/Core/Components/__init__.lua
+++ script/Modules/Core/Components/__init__.lua
@@ -2,4 +2,5 @@ return {
     Children = require('Modules.Core.Components.ChildrenComponent'),
     Parent = require('Modules.Core.Components.ParentComponent'),
     Seed = require('Modules.Core.Components.SeedComponent'),
+    Tag = require('Modules.Core.Components.TagComponent'),
 }
