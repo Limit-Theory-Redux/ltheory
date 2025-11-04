ce4c6024447194ca9a97709a4fac1bcb4ab68b23
Oleg Khryptul <okreptul@yahoo.com>
2025-11-04 21:55:07 +0100

    Consolidate ShaderVar classes

--- script/Shared/Rendering/AutoShaderVar.lua
+++ script/Shared/Rendering/AutoShaderVar.lua
@@ -1,21 +1,41 @@
 local UniformFuncs = require("Shared.Rendering.UniformFuncs")
+
 ---@class AutoShaderVar
 ---@field uniformName string
 ---@field uniformInt integer
 ---@field uniformType UniformType
----@field callbackFn function
----@field perInstance boolean
+---@field callbackFn function|nil
+---@field perInstance boolean|nil
+---@field requiresEntity boolean|nil
 
 ---@class AutoShaderVar
----@overload fun(self: AutoShaderVar, uniformName: string, uniformType: UniformType, callbackFn: function): AutoShaderVar class internal
----@overload fun(uniformName: string, uniformType: UniformType,  callbackFn: function): AutoShaderVar class external
-local AutoShaderVar = Class("AutoShaderVar", function(self, uniformName, uniformType, callbackFn, perInstance)
+---@overload fun(self: AutoShaderVar, uniformName: string, uniformType: UniformType, callbackFn: function|nil, perInstance: boolean, requiresEntity: boolean|nil): AutoShaderVar class internal
+---@overload fun(uniformName: string, uniformType: UniformType,  callbackFn: function|nil, perInstance: boolean, requiresEntity: boolean|nil): AutoShaderVar class external
+local AutoShaderVar = Class("AutoShaderVar", function(self, uniformName, uniformType, callbackFn, perInstance, requiresEntity)
     self.uniformName = uniformName
     self.uniformInt = nil
     self.uniformType = uniformType
     self.callbackFn = callbackFn
     self.perInstance = perInstance or false
+    self.requiresEntity = requiresEntity or false
 end)
+
+---@param callbackFn function
+function AutoShaderVar:setCallbackFn(callbackFn)
+    self.callbackFn = callbackFn
+end
+
+---@param shader Shader
+function AutoShaderVar:setUniformInt(shader)
+    if shader:hasVariable(self.uniformName) then
+        self.uniformInt = shader:getVariable(self.uniformName)
+        return true
+    else
+        Log.Warn("Shader " .. tostring(shader) .. ": Does not have uniform: " .. self.uniformName)
+        self.uniformInt = nil
+        return false
+    end
+end
 
 function AutoShaderVar:setShaderVar(eye, shader, entity)
     if not self.uniformInt then
