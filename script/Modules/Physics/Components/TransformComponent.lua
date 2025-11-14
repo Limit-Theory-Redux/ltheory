e13a70a403bb13b66a01828bbac397c87755a274
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2025-11-14 19:46:20 +0100

    feat: transform updating from rb

--- script/Modules/Physics/Components/TransformComponent.lua
+++ script/Modules/Physics/Components/TransformComponent.lua
@@ -7,47 +7,94 @@ local Component = require("Core.ECS.Component")
 ---@field globalPosition Position
 ---@field globalRotation Quat
 ---@field globalScale gameunit
+---@field dirty boolean
 ---@overload fun(self:TransformComponent): TransformComponent subclass internal
 ---@overload fun(): TransformComponent subclass external
 local TransformComponent = Subclass("TransformComponent", Component, function(self)
     self:setComponentName("Transform")
 
-    self.position = Position()
-    self.rotation = Quat.Identity()
+    self.posLocal = Position()
+    self.rotLocal = Quat.Identity()
+    self.scaleLocal = 1.0
+    self.pos = Position()
+    self.rot = Quat.Identity()
     self.scale = 1.0
-    self.globalPosition = Position()
-    self.globalRotation = Quat.Identity()
-    self.globalScale = 1.0
+    self.dirty = false
 end)
 
 ---@param position Position
-function TransformComponent:setPosition(position)
-    self.position = position
+function TransformComponent:setPos(position)
+    self.pos = position
+    self.dirty = true
 end
 
 ---@return Position
-function TransformComponent:getPosition()
-    return self.position
+function TransformComponent:getPos()
+    return self.pos
 end
 
 ---@param rotation Quat
-function TransformComponent:setRotation(rotation)
-    self.rotation = rotation
+function TransformComponent:setRot(rotation)
+    self.rot = rotation
+    self.dirty = true
 end
 
 ---@return Quat
-function TransformComponent:getRotation()
-    return self.rotation
+function TransformComponent:getRot()
+    return self.rot
 end
 
 ---@param scale gameunit
 function TransformComponent:setScale(scale)
     self.scale = scale
+    self.dirty = true
 end
 
 ---@return gameunit
 function TransformComponent:getScale()
     return self.scale
+end
+
+---@param position Position
+function TransformComponent:setPosLocal(position)
+    self.posLocal = position
+    self.dirty = true
+end
+
+---@return Position
+function TransformComponent:getPosLocal()
+    return self.posLocal
+end
+
+---@param rotation Quat
+function TransformComponent:setRotLocal(rotation)
+    self.rotLocal = rotation
+    self.dirty = true
+end
+
+---@return Quat
+function TransformComponent:getRotLocal()
+    return self.rot
+end
+
+---@param scale gameunit
+function TransformComponent:setScaleLocal(scale)
+    self.scaleLocal = scale
+    self.dirty = true
+end
+
+---@return gameunit
+function TransformComponent:getScaleLocal()
+    return self.scaleLocal
+end
+
+function TransformComponent:isDirty()
+    return self.dirty
+end
+
+---@param dirty boolean
+function TransformComponent:setDirty(dirty)
+    self.dirty = dirty
 end
 
 return TransformComponent
