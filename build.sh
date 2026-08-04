20c7ba3bdc97315ff8f17c43235669a1b410cff5
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2026-08-05 01:26:35 +0200

    fix(ci): detect Windows Git Bash variants

--- build.sh
+++ build.sh
@@ -45,29 +45,37 @@ if [[ $run_tests == true && "$OSTYPE" == "linux-gnu"* ]]; then
     run_tests=false
 fi
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+case "$OSTYPE" in
+  darwin*)
     libprefix="lib"
     libsuffix=".dylib"
     binsuffix=""
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ;;
+  linux-gnu*)
     libprefix="lib"
     libsuffix=".so"
     binsuffix=""
-elif [[ "$OSTYPE" == "msys" ]]; then
+    ;;
+  msys*|cygwin*|mingw*)
     libprefix=""
     libsuffix=".dll"
     binsuffix=".exe"
     if [[ -z "$LIBCLANG_PATH" ]]; then
-        if [[ -d "/c/Program Files/LLVM/bin" ]]; then
-            export LIBCLANG_PATH="/c/Program Files/LLVM/bin"
-        elif [[ -d "/c/Program Files (x86)/LLVM/bin" ]]; then
-            export LIBCLANG_PATH="/c/Program Files (x86)/LLVM/bin"
-        else
-            echo "Set the environment variable LIBCLANG_PATH to the path containing clang.exe in your LLVM installation directory, i.e. C:\Program Files\LLVM\bin"
-            exit 1
-        fi
+      if [[ -d "/c/Program Files/LLVM/bin" ]]; then
+        export LIBCLANG_PATH="/c/Program Files/LLVM/bin"
+      elif [[ -d "/c/Program Files (x86)/LLVM/bin" ]]; then
+        export LIBCLANG_PATH="/c/Program Files (x86)/LLVM/bin"
+      else
+        echo "Set the environment variable LIBCLANG_PATH to the path containing clang.exe in your LLVM installation directory, i.e. C:\Program Files\LLVM\bin"
+        exit 1
+      fi
     fi
-fi
+    ;;
+  *)
+    echo "Unsupported platform: OSTYPE=$OSTYPE"
+    exit 1
+    ;;
+esac
 
 if [ -z "$PHX_VERSION" ]; then
     export PHX_VERSION
